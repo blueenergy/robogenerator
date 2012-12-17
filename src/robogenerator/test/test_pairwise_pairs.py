9bf9522cf21edba5f0b91bae3a3d7f568cf2291e
shuyolin <shuyong.y.lin@nsn.com>
2012-12-17 21:19:39 +0800

    fix a bug when pairwise calculation

--- src/robogenerator/test/test_pairwise_pairs.py
+++ src/robogenerator/test/test_pairwise_pairs.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 sys.path.append('../')
-from common_kw import get_all_pairs
+from data_algorithm.all_pairs import get_all_pairs
 old_parameters = {'unit_type': [ "DMCU"]
              , 'state':[ "WO-EX"]
              , 'restart_mode':[ "OPT","TOT","DSK"]
@@ -12,8 +12,16 @@ old_parameters = {'unit_type': [ "DMCU"]
 class PairwiseTestCase(unittest.TestCase):
     def test_pairwise_pairs_1(self):
         parameters_1 = {'unit_type': [ "DMCU"],'state':[ "WO-EX"]}
-        expected_result = {'unit_type': "DMCU",'state':"WO-EX"}
+        expected_result = [{'unit_type': "DMCU",'state':"WO-EX"}]
         self.assertEqual(expected_result,get_all_pairs(parameters_1))
+        
+        
+    def test_pairwise_pairs_2(self):
+        parameters = {'a':['pikku_a', 'iso_a'],
+               'b':['foo', 'bar']}
+        expected_result = [{'a': "pikku_a",'b':"foo"},{'a': "iso_a",'b':"foo"}
+                           ,{'a': "iso_a",'b':"bar"},{'a': "pikku_a",'b':"bar"}]
+        self.assertEqual(expected_result,get_all_pairs(parameters))
 
 
 
