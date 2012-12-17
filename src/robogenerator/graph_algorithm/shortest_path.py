03d478f24c11d09f36c99d325d1d68b7ba27bdf5
shuyolin <shuyong.y.lin@nsn.com>
2012-12-17 10:48:41 +0800

    copy relevant java class to relevant working directory

--- src/robogenerator/graph_algorithm/shortest_path.py
+++ src/robogenerator/graph_algorithm/shortest_path.py
@@ -1,7 +1,9 @@
 
 #import graph_algorithm.CPP as CPP
 import random
-
+import glob
+import shutil
+import os
 '''
 def compute_cpp_optimal_route(state_name_list,all_possible_transitions):
     G = CPP(len(state_name_list))
@@ -29,6 +31,21 @@ def compute_cpp_optimal_route(state_name_list,all_possible_transitions):
     return optimized_transition
 
 '''
+
+
+
+
+def get_current_java_version():
+    java_version_info = os.getenv('JAVA_HOME','')
+
+    if java_version_info.count('1.7') > 0:
+        java_version = '1.7'
+    elif java_version_info.count('1.6') > 0:
+        java_version = '1.6'
+    else:
+        raise Exception, 'java seems not installed, pls install java at first to run this algorithm'
+    return java_version
+
 def compute_cpp_optimal_route(state_name_list,all_possible_transitions):
 
     arcs =[str(len(state_name_list))]
@@ -45,6 +62,14 @@ def compute_cpp_optimal_route(state_name_list,all_possible_transitions):
     current_path = os.path.abspath('.')
 
     os.chdir(os.path.dirname(__file__))
+    java_version = get_current_java_version()
+    
+    files_to_copied = glob.glob('%s/*.class'%java_version)
+    for class_file in files_to_copied:
+
+        base_name = os.path.basename(class_file)
+        shutil.copyfile(class_file, base_name)
+        
     command_args = ['java','CPP']
     command_args.extend(arcs)
     #p = subprocess.Popen(command_args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -77,6 +102,14 @@ def compute_cpp_optimal_route(state_name_list,all_possible_transitions):
 
 
 if __name__=='__main__':
+    java_version = get_current_java_version()
+
+    files_to_copied = glob.glob('%s/*.class'%java_version)
+    for class_file in files_to_copied:
+
+        base_name = os.path.basename(class_file)
+        shutil.copyfile(class_file, base_name)
+    
     from subprocess import list2cmdline
     command_args =['java', 'CPP', '8', '"0->1 0 1 1"', '"0->6 0 6 1"', '"1->3 1 3 1"', '"1->0 1 0 1"',\
                     '"1-2 1 2 1"', '"2->6 2 6 1"', '"2->1 2 1 1"', '"3->4 3 4 1"', '"3->7 3 7 1"', '"4->5 4 5 1"',\
@@ -95,8 +128,10 @@ if __name__=='__main__':
     
     command_string = list2cmdline(command_args)
     print command_string
+    '''
     #res = os.system(command_string)
     res = os.popen(command_string).readlines()
     print res
+    '''
 
 
